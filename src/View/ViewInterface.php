4e0644eb12e587e923d3610608e05308bb2ae0d9
titrxw <1120309488@qq.com>
2020-11-13 18:27:30 +0800

    修改接口

--- src/View/ViewInterface.php
+++ src/View/ViewInterface.php
@@ -1,11 +1,22 @@
 <?php
 
+/**
+ * Rangine Contract
+ *
+ * (c) We7Team 2019 <https://www.rangine.com>
+ *
+ * document http://s.w7.cc/index.php?c=wiki&do=view&id=317&list=2284
+ *
+ * visited https://www.rangine.com for more details
+ */
+
 namespace W7\Contract\View;
 
 interface ViewInterface {
 	public function registerFunction($name, \Closure $callback);
 	public function registerConst($name, $value);
 	public function registerObject($name, $object);
+	public function addTemplatePath(string $namespace, string $path);
 
 	public function render($name, $context = []) : string;
 }
