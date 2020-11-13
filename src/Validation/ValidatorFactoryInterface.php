a4eec6a51352f09cce8f88d173d9df4fc3b62fda
titrxw <1120309488@qq.com>
2020-11-13 17:35:02 +0800

    修改validationFactory

--- src/Validation/ValidatorFactoryInterface.php
+++ src/Validation/ValidatorFactoryInterface.php
@@ -1,9 +1,28 @@
 <?php
 
+/**
+ * Rangine Contract
+ *
+ * (c) We7Team 2019 <https://www.rangine.com>
+ *
+ * document http://s.w7.cc/index.php?c=wiki&do=view&id=317&list=2284
+ *
+ * visited https://www.rangine.com for more details
+ */
+
 namespace W7\Contract\Validation;
 
 use Illuminate\Contracts\Validation\Factory;
 
 interface ValidatorFactoryInterface extends Factory {
-
+	/**
+	 * Create a new Validator instance.
+	 *
+	 * @param  array  $data
+	 * @param  array  $rules
+	 * @param  array  $messages
+	 * @param  array  $customAttributes
+	 * @return ValidatorInterface
+	 */
+	public function make(array $data, array $rules, array $messages = [], array $customAttributes = []) : ValidatorInterface;
 }
