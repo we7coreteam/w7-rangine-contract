efcb48484bd57dad9582b44a0c796e8ed823477d
titrxw <1120309488@qq.com>
2020-11-16 16:21:33 +0800

    修改eventDispatcher接口

--- src/Event/EventDispatcherInterface.php
+++ src/Event/EventDispatcherInterface.php
@@ -1,9 +1,18 @@
 <?php
 
+/**
+ * Rangine Contract
+ *
+ * (c) We7Team 2019 <https://www.rangine.com>
+ *
+ * document http://s.w7.cc/index.php?c=wiki&do=view&id=317&list=2284
+ *
+ * visited https://www.rangine.com for more details
+ */
+
 namespace W7\Contract\Event;
 
 use Illuminate\Contracts\Events\Dispatcher;
 
-interface EventDispatcherInterface extends Dispatcher {
-
+interface EventDispatcherInterface extends Dispatcher, \Psr\EventDispatcher\EventDispatcherInterface {
 }
