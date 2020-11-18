338becfd1868491ba3c12ee504b203dc453c362e
titrxw <1120309488@qq.com>
2020-11-18 14:20:54 +0800

    修改session interface

--- src/Session/SessionInterface.php
+++ src/Session/SessionInterface.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * Rangine Contract
+ *
+ * (c) We7Team 2019 <https://www.rangine.com>
+ *
+ * document http://s.w7.cc/index.php?c=wiki&do=view&id=317&list=2284
+ *
+ * visited https://www.rangine.com for more details
+ */
+
 namespace W7\Contract\Session;
 
 interface SessionInterface {
@@ -12,4 +22,5 @@ interface SessionInterface {
 	public function delete($keys);
 	public function destroy();
 	public function close();
+	public function gc();
 }
