ebf2636b560475b5921ffcd02168d979a5847d0e
titrxw <1120309488@qq.com>
2021-05-07 11:21:49 +0800

    修改route interface

--- src/Router/RouteInterface.php
+++ src/Router/RouteInterface.php
@@ -17,7 +17,7 @@ use Psr\Http\Message\RequestInterface;
 interface RouteInterface {
 	public function getName();
 	public function getModule();
-	public function getUrl();
+	public function getUri();
 	public function getController();
 	public function getAction();
 	public function getArgs();
