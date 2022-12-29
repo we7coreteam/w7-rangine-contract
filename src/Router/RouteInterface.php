51491663436cf99a2dea86ef7ccf78e259860d15
rxwyun <1120309488@qq.com>
2022-12-29 17:51:17 +0800

    修改routerinterface

--- src/Router/RouteInterface.php
+++ src/Router/RouteInterface.php
@@ -1,13 +1,13 @@
 <?php
 
 /**
- * Rangine Contract
+ * This file is part of Rangine
  *
- * (c) We7Team 2019 <https://www.rangine.com>
+ * (c) We7Team 2019 <https://www.rangine.com/>
  *
  * document http://s.w7.cc/index.php?c=wiki&do=view&id=317&list=2284
  *
- * visited https://www.rangine.com for more details
+ * visited https://www.rangine.com/ for more details
  */
 
 namespace W7\Contract\Router;
@@ -23,5 +23,6 @@ interface RouteInterface {
 	public function getArgs();
 	public function getDefaults();
 	public function getMiddleware() : array;
+	public function getOption() : array;
 	public function run(RequestInterface $request);
 }
